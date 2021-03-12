1124eea21db7ccf76b0429d7cab8da23574e0ba0
DragonFireJ <63804852+DragonFireJ@users.noreply.github.com>
2021-03-12 08:16:29 -0600

    Add files via upload

---
+++ basicos.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basicos</title>
+	<meta charset="utf-8">
+</head>
+	<style>
+		  
+      * {
+        margin:0px;
+        padding:0px;
+      }
+      
+      #header {
+        margin:auto;
+        width:100%;
+        font-family:Arial, Helvetica, sans-serif;
+      }
+   
+      ul, ol {
+        list-style:none;
+      }
+      
+      .nav > li {
+        float:left;
+      }
+
+      
+      .nav li a {
+        background-color:#343588;
+        color:#fff;
+        text-decoration:none;
+        padding:10px 133px;
+        display:block;
+      }
+      
+      .nav li a:hover {
+        background-color:#E15959;
+      }
+      
+      .nav li ul {
+        display:none;
+        position:absolute;
+        min-width:130px;
+      }
+      
+      .nav li:hover > ul {
+        display:block;
+      }
+      
+      .nav li ul li {
+        position:relative;
+      }
+      
+      .nav li ul li ul {
+        right:-140px;
+        top:0px;
+      }
+      footer{
+        position: relative;
+        margin-top: -10px;
+        width: 100%;
+        padding: 50px;
+        clear: both;
+        background-color: #68A6C0;
+        text-align: center;
+
+      }
+h2{
+	margin : 0;
+	padding: 2em;
+}
+.container{
+	background-color: #A5A6E8;
+	padding: 1em;
+}
+.item{
+	margin: 5em;
+	padding: 3em;
+	color: #ffffff;
+	float: left;
+}
+#item1{
+	background-color: #a05ce4;
+}
+#item2{
+	background-color: #5ca0e4; 
+}
+@keyframes cambio {
+    0% {margin-left: 0;}
+    20% {margin-left: 0;}
+    
+    25% {margin-left: -100%;}
+    45% {margin-left: -100%;}
+    
+    50% {margin-left: -200%;}
+    70% {margin-left: -200%;}
+
+    75% {margin-left: -300%;}
+    100% {margin-left: -300%;}
+}
+
+	</style>
+</head>
+ <body bgcolor="#6667B4">
+
+                        <br>
+                     <center><font face="Arial,verdana" size="6" color="white"><strong>TIPO DE TRÁMITES DISPONIBLE</font></center></strong> 
+                     <br><br>
+                     <div class="container"></div>
+                     <div class="item" id="item1">
+                     	<img src="icono.jpg"  height="180" width="180">
+                     <b>	<p><center>KÁRDEX</center></p></b>
+                     </div>
+                     <div class="item" id="item1">
+                      <img src="medalicon5.png"  height="180" width="180">
+                      <p><center>ADEUDOS</center></p>
+                     </div>
+                     <div class="item" id="item1">
+                     	<img src="12.png"  height="180" width="180">
+                     	<p><center>CONSTANCIAS</center></p>
+                     </div>
+                     <div class="item" id="item1">
+                     	<img src="tit.jpg"  height="180" width="210">
+                     	<p><center>TRÁMITES DE TITULACIÓN </center></p>
+                     </div>
+                     <div class="item" id="item1">
+                     	<img src="C1.png"  height="180" width="180">
+                     	<p><center>CREDENCIALIZACIÓN</center></p>
+                     </div>
+                     <div class="item" id="item1">
+                     	<img src="C2.png"  height="180" width="180">
+                     	<p><center>BAJAS DE MATERIAS</center></p>
+                     </div>
+                     <div class="item" id="item1">
+                     	<img src="R1.png"  height="180" width="180">
+                     	<p><center>DUDAS</center></p>
+                     </div>
+                     <div class="item" id="item1">
+                     	<img src="C7.png"  height="180" width="180">
+                     	<p><center>INSCRIPCIONES</center></p>
+                     </div>
+                     <div class="item" id="item1">
+                     	<img src="T1.png"  height="180" width="180">
+                     	<p><center>TALLERES EDUCATIVOS</center></p>
+                     </div>
+                   
+                     <br><br>
+</html>
